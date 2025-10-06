c4b92c7969e85e00f5ba1bb85c4edfd0f2c733ab
JOHN RAFAEL ALEJANDRINO <167075750+jeeracd@users.noreply.github.com>
2025-10-06 22:39:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy as np
 
-messy_dataset = pd.read_csv('messy_dataset.csv')
+messyDataset = pd.read_csv('messy_dataset.csv')
 
+emptyCells = messyDataset.isnull().sum()
+#print(empty_cells) # Prints number of missing values in each column
+
+messyDataset["Income"] = messyDataset["Income"].str.replace('k', '').str.replace(',', '').str.replace('K', '').astype(float) * 1000
+
+incomeMode = messyDataset["Income"].mode()[0]
+messyDataset["Income"] = messyDataset["Income"].apply(lambda x : incomeMode if x < 0 else x)
+
+incomeAvg = messyDataset["Income"].mean()
+
+print(f"Average Income: {incomeAvg}")
