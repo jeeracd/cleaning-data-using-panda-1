d487fb27a7afb7283570314e9908dddcd8e4a227
JOHN RAFAEL ALEJANDRINO <167075750+jeeracd@users.noreply.github.com>
2025-10-07 22:49:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,24 @@ incomeMode = messyDataset["Income"].mode()[0]
 messyDataset["Income"] = messyDataset["Income"].apply(lambda x : incomeMode if x < 0 else x)
 
 incomeAvg = messyDataset["Income"].mean()
+incomestd = messyDataset["Income"].std()
 
-#print(f"Average Income: {incomeAvg}")
+print(f"Average Income: {incomeAvg}")
+print(f"Income Standard Deviation: {incomestd}")
 
+
+messyDataset["Purchase_Amount"] = (messyDataset["Purchase_Amount"].astype(str).str.replace('$', '').str.replace(',', '').str.strip())
+messyDataset["Purchase_Amount"] = pd.to_numeric(messyDataset["Purchase_Amount"], errors='coerce')
+purchaseAmountMedian = messyDataset["Purchase_Amount"].median()
+messyDataset["Purchase_Amount"] = messyDataset["Purchase_Amount"].fillna(purchaseAmountMedian)
+paMin = messyDataset["Purchase_Amount"].min()
+paMax = messyDataset["Purchase_Amount"].max()  
+
+
+messyDataset["Purchase_Amount"] = ((messyDataset["Purchase_Amount"] - paMin) / (paMax - paMin))
+print(messyDataset["Purchase_Amount"].describe())
+
+'''
 emptyCellsPct = (emptyCells / len(messyDataset)) * 100
 #print(emptyCellsPct)
 
@@ -43,4 +58,4 @@ messyDataset["Purchase_Amount"] = pd.to_numeric(messyDataset["Purchase_Amount"],
 median = messyDataset["Purchase_Amount"].median()
 messyDataset["Purchase_Amount"] = messyDataset["Purchase_Amount"].fillna(median)
 messyDataset["Purchase_Amount"] = messyDataset["Purchase_Amount"].astype(float)
-print(messyDataset["Purchase_Amount"].describe())
+print(messyDataset["Purchase_Amount"].describe())'''
